2a986742d1bc73506dbdf2aee44e5e06c2e3f37a
ZinniOggy <moomtahina@iut-dhaka.edu>
2024-12-31 11:02:04 +0600

    Update gift.js

--- gift.js
+++ gift.js
@@ -1,6 +1,6 @@
-HEAD
+
 function gift(){
     document.getElementById("giftBox").src="letterop.jpg";
     document.getElementById("message").innerHTML = "2025"
-762e81d408bb80a9a894b63eff1114c83d6bb8d2
+
 }
